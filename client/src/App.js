3b6c8d45ff047c6e15e106b86130ff2e8150240e
chazzin chazzin <csihom@iastate.edu>
2024-09-09 16:13:17 -0500

    Added page when a group is clicked on

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { Home, Login } from './components'
+import { Home, Login, GroupView } from './components'
 
 const App = () => {
     return (
@@ -8,6 +8,7 @@ const App = () => {
             <Routes>
                 <Route path='/login' element={<Login />} />
                 <Route path='/*' element={<Home />} />
+                <Route path='/Group' element= {<GroupView />} />
             </Routes>
         </div>
     )
---
+++ client/src/components/GroupView.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const GroupView = () => {
+    return (
+        <div>GroupView</div>
+    )
+}
+
+export default GroupView;
