faacc9486b33d6659bc535d1275dcc02a91751f1
Phuong H Nguyen <torao@Phuongs-MacBook-Air.local>
2024-09-17 19:28:34 -0500

    protect firebase keys

--- client/src/config/firebase.config.js
+++ client/src/config/firebase.config.js
@@ -3,12 +3,12 @@ import { getStorage } from "firebase/storage"
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBNeCUneDpzQSqe89xeixwzLt1Y6JEV7m8",
-    authDomain: "iot-dashboard-5cbbe.firebaseapp.com",
-    projectId: "iot-dashboard-5cbbe",
-    storageBucket: "iot-dashboard-5cbbe.appspot.com",
-    messagingSenderId: "127245578470",
-    appId: "1:127245578470:web:d1b464b1266f48367c4b27"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSEAGING_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
