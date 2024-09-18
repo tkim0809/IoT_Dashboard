0ef2b905e5079f0b33f762df852368afdef0015f
Phuong H Nguyen <torao@Phuongs-MacBook-Air.local>
2024-09-18 00:35:57 -0500

    create api endpoint

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,19 +1,35 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const { onRequest } = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");  // Optional logger
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const admin = require("firebase-admin");
+require('dotenv').config();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const serviceAccountKey = require('./serviceAccountKey.json');
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const express = require('express');
+const app = express();
+
+// Body parser for our JSON data
+app.use(express.json());
+
+// Cross-origin setup
+const cors = require("cors");
+app.use(cors({ origin: true }));
+
+app.use((req, res, next) => {
+    res.set("Access-Control-Allow-Origin", "*");
+    next();
+});
+
+// Firebase credentials
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccountKey)
+});
+
+// API endpoints
+app.get("/", (req, res) => {
+    return res.send("Hello world");
+});
+
+// Export using v2 trigger
+exports.app = onRequest(app);
