207aca45f44917924ab2f6e83a6408c08b8944d7
chazzin chazzin <csihom@iastate.edu>
2024-09-23 18:58:50 -0500

    Refined the visuals of data table

--- client/src/components/GroupView.jsx
+++ client/src/components/GroupView.jsx
@@ -15,7 +15,7 @@ const GroupView = () => {
     const [passedData, setPassedData] = useState(null);
 
     const handleExpand = (type, name, data) => {
-        if (type == "airlines") {
+        if (type === "airlines") {
             setAirlinesExpand(true);
         }
         else {
@@ -38,26 +38,27 @@ const GroupView = () => {
     }
 
     return (
-        <div id="groupview_container" className="bg-white w-screen h-screen flex flex-wrap p-10" style={{ justifyContent: "center" }}>
+        <div id="groupview_container" className="bg-white w-screen h-screen flex flex-wrap p-10" style={{ justifyContent: "center", overflowX: "hidden" }}>
             <div style={{ width: "100%", display: "flex", flexDirection: "row" }}>
-                <img className="border-solid border-2 border-black" style={{ width: "100px", height: "100px" }} src="" />
+                <img className="border-solid border-2 border-black" style={{ width: "100px", height: "100px" }} src="" alt="" />
                 <h1 style={{ width: "100%", textAlign: "center" }}>Group name</h1>
             </div>
-            <div id="table_container" className="border-solid border-2 border-black" style={{ width: "100%", height: "60%", marginTop: "100px" }}>
+            <div id="table_container" className="border-solid border-2 border-black border-opacity-25" style={{borderWidth: "1px", width: "100%", height: "499px", marginTop: "100px", borderRadius: "10px" }}>
                 {/* Div of buttons that will change what is currently being sorted */}
-                <div className="flex-initial flex-row border-black border-1 border-solid h-15 border-b-0" style={{ width: "100%" }}>
-                    <button className="w-20" onClick={() => setSortCategory("all")} style={{ color: sortCategory === "all" ? "blue" : "black" }}>All</button>
-                    <button className="w-20" onClick={() => setSortCategory("airlines")} style={{ color: sortCategory === "airlines" ? "blue" : "black" }}>Airlines</button>
-                    <button className="w-20" onClick={() => setSortCategory("fleet")} style={{ color: sortCategory === "fleet" ? "blue" : "black" }}>Fleet</button>
-                    <button className="w-20" onClick={() => setSortCategory("subfleet")} style={{ color: sortCategory === "subfleet" ? "blue" : "black" }}>Sub Fleet</button>
-                    <button className="w-20" onClick={() => setSortCategory("aircraft")} style={{ color: sortCategory === "aircraft" ? "blue" : "black" }}>Aircraft</button>
+                <div className="flex-initial flex-row h-15 py-2" style={{ width: "100%" }}>
+                    <button className="w-20" onClick={() => setSortCategory("all")} style={{ color: sortCategory === "all" ? "blue" : "black", borderBottom: sortCategory === "all" ? "solid 2px blue" : "none" }}>All</button>
+                    <button className="w-20" onClick={() => setSortCategory("airlines")} style={{ color: sortCategory === "airlines" ? "blue" : "black", borderBottom: sortCategory === "airlines" ? "solid 2px blue" : "none" }}>Airlines</button>
+                    <button className="w-20" onClick={() => setSortCategory("fleet")} style={{ color: sortCategory === "fleet" ? "blue" : "black", borderBottom: sortCategory === "fleet" ? "solid 2px blue" : "none" }}>Fleet</button>
+                    <button className="w-20" onClick={() => setSortCategory("subfleet")} style={{ color: sortCategory === "subfleet" ? "blue" : "black", borderBottom: sortCategory === "subfleet" ? "solid 2px blue" : "none" }}>Sub Fleet</button>
+                    <button className="w-20" onClick={() => setSortCategory("aircraft")} style={{ color: sortCategory === "aircraft" ? "blue" : "black",borderBottom: sortCategory === "aircraft" ? "solid 2px blue" : "none" }}>Aircraft</button>
                 </div>
+
                 {/* table of list of fleets, subfleets, aircrafts, and airlines */}
-                <table className="min-w-full min-h-20 border border-collapse">
-                    <thead>
+                <table className="min-w-full" style={{height: "calc(100%-60px)"}}>
+                    <thead style={{backgroundColor: "#86a7b5"}}>
                         <tr>
-                            <th className="border border-gray-600 px-4 py-2" style={{ textAlign: "left" }}>Fleet Name</th>
-                            <th>Expand</th>
+                            <th className="border-t border-b border-l border-gray-600 px-4 py-4 border-opacity-25" style={{borderWidth: "1px", textAlign: "left", color: "white"}}>Fleet Name</th>
+                            <th className="border-t border-b border-r border-gray-600 px-4 py-4 border-opacity-25" style={{color: "white"}}>Airline</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -66,10 +67,11 @@ const GroupView = () => {
                     airlines
                     .slice(itemIndex, itemIndex + 5)
                     .map(data => (
-                        <tr key={data.name}>
-                        <td className="border border-gray-600 px-4 py-2" style={{ width: "75%" }}>{data.name}</td>
-                        <td className="border border-gray-600 px-4 py-2 text-center">
-                            <button onClick={() => handleExpand("airlines", data.name, data.aircrafts)}>Expand</button>
+                        <tr className="px-4 py-2 cursor-pointer" 
+                        key={data.name} 
+                        onClick={() => handleExpand("airlines", data.name, data.aircrafts)}>
+                        <td style={{ width: "100%" }}>
+                            {data.name}
                         </td>
                         </tr>
                     ))
@@ -78,22 +80,20 @@ const GroupView = () => {
                     //If sorted category is not airlines it will map over data for either all, fleets, subfleets, or aircrafts
                     .filter(item => sortCategory === "all" || item.fleetCategory === sortCategory)
                     .slice(itemIndex, itemIndex + 5)
-                    .map(item => (
-                        <tr key={item.fleetName}> 
-                        <td className="border border-gray-600 px-4 py-2" style={{ width: "75%" }}>
+                    .map((item, index) => (
+                    <tr className="cursor-pointer" style={{height: "20%"}}
+                        key={item.fleetName} 
+                        onClick={() => handleExpand("all", item.fleetName, "To be implemented")}>
+                        <td className="h-20 border-opacity-25 border-t" style={{ borderRight: "none", width: "80%", padding: "10px", borderWidth: "1px", backgroundColor: index % 2 === 0 ? "white" : "#ecf2f3" }}>
                             {item.fleetName}
                         </td>
-                        <td className="border border-gray-600 px-4 py-2 text-center">
-                            <button onClick={() => handleExpand("all", item.fleetName, "Too be implemented")}>Expand</button>
-                        </td>
-                        </tr>
+                        <td className="h-20 border-opacity-25 border-t" style={{textAlign: "center", borderLeft: "none", padding: "10px", borderWidth: "1px", backgroundColor: index % 2 === 0 ? "white" : "#ecf2f3"}}>Delta</td>
+                    </tr>
                     ))
                     )}
                     </tbody>
                 </table>
             </div>
-            {/* Button for pagination */}
-            <button onClick={handleNextPage}>Next Page</button>
             {/* Opens display for expansion to view data */}
             {isAirlinesExpanded 
                 ? <ExpandedAirlines fleetName={fleetName} data={passedData} onClose={handleAirlineClose} />
